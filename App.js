de622ac236fb01edb4b625828f4b36788ab6929e
Rajeshdreams333 <rajeshdreams333@gmail.com>
2023-06-10 22:27:43 +0530

    this is my third commit

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 /*
-        HOT MODULE RELOADING (HMR) =>IT USE WHEN EVER WE CHANCE IN CSS OR HTML CHANGES TAKE PLACE.
+      what is bundler?
+      help optimize code delivery, improve performance, and enable the use of advanced JavaScript features.
+
+        HOT MODULE REPLACEMENT (HMR) =>IT USE WHEN EVER WE CHANCE IN CSS OR HTML CHANGES TAKE PLACE.
         File Watcher algorithm => (which type of file is this.) - C++.
         Bundling
         Minify
@@ -11,34 +14,83 @@
         Compression
         Compatble with older version of browser
         Https on dev
-        
+        port number
+        Consistent Hashing algorithm
+        Zero Config
+        Tree shaking -- remove unwanted code
+
+        Transitive Deopendencies
 */
-
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title", 
-  },
-  "Hi Rajesh...."
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//     key:"h1",
+//   },
+//   "Hi Rajesh...."
+// );
+// const heading2 = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//     key:"h2",
+//   },
+//   "Hello Frnds...."
+// );
+// const container = React.createElement(
+//   "div",
+//   {
+//     id: "container",
+//   },
+//   [heading, heading2]
+// );
+// console.log(container);
+// const heading=React.createElement(
+//   "h1",
+//   {
+//     id:"title",
+//     key:"h1"
+//   },
+//   "Namaste Rajesh...."
+// );
+
+// =================================================
+const heading2 = (
+  <h1 id="id1" key="h1">
+    Hello Everyone....
+  </h1>
 );
-const heading2 = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Hello Frnds...."
+
+// React component
+// functional component  -- New
+// class based component -- Old
+const HeaderComponent1 = () => (
+  <div>
+  <h1>
+    I will try my level best....
+  </h1>
+  </div>
 );
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading2]
-);
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <HeaderComponent1 />
+      {HeaderComponent1()}
+      {heading2} 
+      <h1>Hi Friends.... This is a functional component</h1>
+      <h1>This is my heading2</h1>
+    </div>
+  );
+};
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<HeaderComponent />);
+
+
+// When we try to render the component make it as tags ==><component_name />
